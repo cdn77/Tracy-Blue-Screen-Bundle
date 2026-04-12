0e4c10bc64dfc136b655cc0b8b813bc275a3ef53
Simon Podlipsky <simon@podlipsky.net>
2026-04-12 11:49:46 +0200

    Revert "fix(test): reset static BlueScreen scrubber in setUp for test isolation"
    This reverts commit dedcd50962614f33ce1281a877448fa97cc13dbe.

--- tests/DependencyInjection/TracyBlueScreenExtensionTest.php
+++ tests/DependencyInjection/TracyBlueScreenExtensionTest.php
@@ -11,7 +11,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Tracy\BlueScreen;
-use Tracy\Debugger;
 
 use function array_key_exists;
 use function assert;
@@ -74,9 +73,6 @@ final class TracyBlueScreenExtensionTest extends AbstractExtensionTestCase
         $this->setParameter('kernel.cache_dir', __DIR__ . '/tests-cache-dir');
         $this->setParameter('kernel.environment', 'dev');
         $this->setParameter('kernel.debug', true);
-
-        // Reset the static BlueScreen's scrubber to ensure test isolation
-        Debugger::getBlueScreen()->scrubber = null;
     }
 
     public function testOnlyAddCollapsePaths(): void
